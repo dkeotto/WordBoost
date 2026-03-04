c1919d523d4d86242c6d87f667e5082008bd1252
dkeotto <dorukyagiz34@gmail.com>
2026-03-04 17:39:30 +0300

    fix server double listen

--- server.js
+++ server.js
@@ -472,9 +472,3 @@ process.on('unhandledRejection', (reason, promise) => {
   console.error('Unhandled Rejection at:', promise, 'reason:', reason);
 });
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, '0.0.0.0', () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
-
